5ddc68c068fae8b0c257bc6cb2793e08548b3ff2
unknown <trunghieu20032502@gmail.com>
2024-10-02 19:48:15 +0700

    update commit

--- back-end/src/api/v1/routes/index.route.js
+++ back-end/src/api/v1/routes/index.route.js
@@ -1,10 +1,9 @@
+const homeRouter = require("./home.route");
 const urlRouter = require("./url.route");
 const authRouter = require("./user.route");
 
-module.exports = (app) => {
-    app.use("/", async (req, res) => {
-        res.send("Server listening...");
-    })
+module.exports = (app) => { 
+    app.use("/", homeRouter);
     app.use("/api/url", urlRouter);
     app.use("/api/auth", authRouter);
 };
