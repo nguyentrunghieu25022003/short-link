a5ba33cf630a30ad927525b4700233e03cba9e56
unknown <trunghieu20032502@gmail.com>
2024-10-18 19:08:10 +0700

    update commit

--- front-end/src/pages/home/index.jsx
+++ front-end/src/pages/home/index.jsx
@@ -69,6 +69,10 @@ const Home = () => {
     })();
   }, [userId, isRefreshing]);
 
+  useEffect(() => {
+    window.location.reload();
+  }, []);
+
   if (isLoading) {
     return <Loading />;
   };
